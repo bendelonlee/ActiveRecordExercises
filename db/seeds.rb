41cfd3c4973c024e24700178de960bbca50b4b66
Ben Lee <bendelonlee@gmail.com>
2019-08-15 10:18:02 -0600

    Adds exercise 'All courses ordered by name'. Works when walked through.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,26 @@ include FactoryBot::Syntax::Methods
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Completion.destroy_all
+User.destroy_all
 Exercise.destroy_all
-Exercise.create!(name: "all", solution: "Course.all", instruction: "Write a query to return all Course models from the database.")
-Exercise.create!(name: "average grades",
-                solution: 'Student.select("name, avg(enrollments.grade) as average_grade").joins(:enrollments).group(:id)',
-                instruction: "Write a query to return all students names and their average grades (as an attribute called average_grade) in this column order.")
 
-Exercise.create!(name: "top grades",
-                 solution: 'Student.select(:name, "AVG(enrollments.grade) AS average_grade").joins(:enrollments).group(:id).having("AVG(enrollments.grade) > (SELECT AVG(grade) FROM enrollments)")',
-                 instruction: "Write a query to return all students names and their average grades (as an attribute called average_grade) in this column order, but only for students whose average grade is above the average of all grades")
-      
+Exercise.create!(name: "All courses",
+                 solution: "Course.all",
+                 instruction: "Write a query to return all Course models from the database.",
+                 notes: "Easy peasy.")
+Exercise.create!(name: "All courses ordered by name",
+                solution: "Course.order(name: :asc)",
+                instruction: "Write a query to return all Course models from the database in ascending alphabetical order by name.",
+                notes: "It is possible to write Course.order(:name) and the data will be returned in ascending order by default. However, if you care what order your results are returned in, it's better practice to be specific.")
+
+
+# Commenting out advanced exercises for the time being
+# Exercise.create!(name: "average grades",
+#                 solution: 'Student.select("name, avg(enrollments.grade) as average_grade").joins(:enrollments).group(:id)',
+#                 instruction: "Write a query to return all students names and their average grades (as an attribute called average_grade) in this column order.")
+#
+# Exercise.create!(name: "top grades",
+#                  solution: 'Student.select(:name, "AVG(enrollments.grade) AS average_grade").joins(:enrollments).group(:id).having("AVG(enrollments.grade) > (SELECT AVG(grade) FROM enrollments)")',
+#                  instruction: "Write a query to return all students names and their average grades (as an attribute called average_grade) in this column order, but only for students whose average grade is above the average of all grades")
+#
