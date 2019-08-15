53ae31e89ba654716f2f5446945e0561213d629e
Ben Lee <bendelonlee@gmail.com>
2019-08-15 10:17:10 -0600

    Adds notes column to the exercise table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190814202749) do
+ActiveRecord::Schema.define(version: 20190815160449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20190814202749) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "notes"
   end
 
   create_table "solutions", force: :cascade do |t|
