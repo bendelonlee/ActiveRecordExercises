62845fe145e120bbbac3d51f81830ed7007634b5
Ben Lee <bendelonlee@gmail.com>
2019-08-16 16:17:22 -0600

    Adds filters to simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,9 @@ SimpleCov.start 'rails' do
   add_filter '/db/'
   add_filter '/spec/' # for rspec
   add_filter '/test/' # for minitest
+  add_filter 'app/channels/'
+  add_filter 'app/jobs/'
+  add_filter 'app/mailers/'
 end
 
 RSpec.configure do |config|
