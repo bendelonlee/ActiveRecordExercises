40545b7c274c5b5ce3bd47a3bc599c3c63348b11
Ben Lee <bendelonlee@gmail.com>
2019-09-01 16:39:21 -0600

    Expands feature test to ensure timedblocks are inserted into the database, that the hours until expiration is displayed that they do not allow users to complete exercises when in effect

--- spec/features/user/exercise_credit_blocked_spec.rb
+++ spec/features/user/exercise_credit_blocked_spec.rb
@@ -7,14 +7,19 @@ describe 'exercise index page' do
     ex1 = create(:exercise, index: 1)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user.reload)
     visit exercise_path(ex1)
-    click_on("#toggle-button")
-    expect(page).to have_content("Because you've seen our answer before writing your own, you must wait 24 hours before your completion will be counted.")
+    expect(TimedBlock.count).to eq(0)
+    click_on("surrender-button") # Selecting by id, but not using a hashtag. Has to do with the particulars of button_to I'm guessing. Using an hashtag fails.
+    expect(TimedBlock.count).to eq(1)
+    tb = TimedBlock.first
+    expect(tb.user).to eq(user)
+    expect(tb.exercise).to eq(ex1)
+    expect(tb.expiration.to_i).to eq(1.days.from_now.to_i)
+    expect(page).to have_content('You will be able to complete this exercise in 24 hours')
     fill_in :solution_solution_code, with: ex1.solution
     click_on 'execute'
-    expect(page).to have_content('You will be able to complete this exercise in 24 hours')
     visit exercises_path
-    within "#exercise-#{ex3.id}" do
-      expect(page).to have_content "Completed 0 times"
+    within "#exercise-#{ex1.id}" do
+      expect(page).to_not have_content "Completed"
     end
   end
 end
