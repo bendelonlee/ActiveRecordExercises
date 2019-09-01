ca4c92dfef6302a78bb3016fc7ca9c431b9e2364
Ben Lee <bendelonlee@gmail.com>
2019-09-01 17:01:09 -0600

    Adds tests in exercise model test for .blocked?, hours_until_unblocked, and ensures an exercises cannot be marked completed if it is blocked

--- spec/models/excercise_spec.rb
+++ spec/models/excercise_spec.rb
@@ -30,8 +30,37 @@ RSpec.describe Exercise, type: :model do
       exercise.mark_completed_by(user)
       completion.reload
       expect(completion.times_completed).to eq(3)
+      TimedBlock.create(exercise: exercise, user: user, expiration: 1.days.from_now)
+      exercise.mark_completed_by(user)
+      completion.reload
+      
+      expect(completion.times_completed).to eq(3)
 
     end
+    describe 'blocked?' do
+      before(:each) do
+        @user = create(:user)
+        @exercise = create(:exercise)
 
+      end
+      scenario 'when no blocks' do
+        expect(@exercise.blocked?(@user)).to eq(false)
+      end
+      scenario 'when expired blocks' do
+        TimedBlock.create!(user: @user, exercise: @exercise, expiration: 1.days.ago)
+        expect(@exercise.blocked?(@user)).to eq(false)
+      end
+      scenario 'when unexpired blocks' do
+        TimedBlock.create!(user: @user, exercise: @exercise, expiration: 1.days.from_now)
+        expect(@exercise.blocked?(@user)).to eq(true)
+      end
+    end
+    it 'hours_until_unblocked' do
+      user = create(:user)
+      exercise = create(:exercise)
+      TimedBlock.create!(user: user, exercise: exercise, expiration: 7.hours.from_now)
+      TimedBlock.create!(user: user, exercise: exercise, expiration: 17.hours.from_now)
+      expect(exercise.hours_until_unblocked(user)).to eq(17)
+    end
   end
 end
