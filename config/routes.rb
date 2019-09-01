67c50c6bd0d9089c65a34b4fc2169a397a6a034c
Ben Lee <bendelonlee@gmail.com>
2019-09-01 17:09:50 -0600

    Adds TimedBlock route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "/", to: "exercises#index", as: "root"
   get '/auth/google_oauth2', as: 'sign_up'
   get '/auth/google_oauth2/callback', to: 'sessions#create'
+  post '/timed_blocks/:exercise_id', to: 'timed_blocks#create', as: 'surrender'
   resources :exercises, only: [:show, :index] do
     resources :solutions, only: [:create]
   end
