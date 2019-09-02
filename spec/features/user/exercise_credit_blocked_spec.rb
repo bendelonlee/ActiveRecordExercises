5a7b454bfca52cba15f29f739e68b3ea6e51f517
Ben Lee <bendelonlee@gmail.com>
2019-09-02 09:20:16 -0600

    Adds feature test for timedblock to be created when an exercise is completed.

--- spec/features/user/exercise_credit_blocked_spec.rb
+++ spec/features/user/exercise_credit_blocked_spec.rb
@@ -22,4 +22,24 @@ describe 'exercise index page' do
       expect(page).to_not have_content "Completed"
     end
   end
+  it 'after I complete an exercise, I am not allowed to complete again for credit for 48 hours' do
+    create(:user)
+    Student.create!(name: 'b')
+    user = create(:user)
+    ex1 = create(:exercise, index: 1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user.reload)
+    visit exercise_path(ex1)
+    expect(TimedBlock.count).to eq(0)
+    fill_in :solution_solution_code, with: ex1.solution
+    click_on 'execute'
+    expect(TimedBlock.count).to eq(1)
+    visit exercise_path(ex1)
+    expect(page).to have_content('You will be able to complete this exercise again in 48 hours')
+    fill_in :solution_solution_code, with: ex1.solution
+    click_on 'execute'
+    visit exercises_path
+    within "#exercise-#{ex1.id}" do
+      expect(page).to have_content "Completed 1 times"
+    end
+  end
 end
