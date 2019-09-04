8a9d51b4f8736db7cd857d3a3b11d11be84da092
Ben Lee <bendelonlee@gmail.com>
2019-09-04 10:56:37 -0600

    Adds logic in controller so only correct queries result in completions. All tests passing

--- spec/features/user/exercise_completion_spec.rb
+++ spec/features/user/exercise_completion_spec.rb
@@ -19,4 +19,19 @@ describe 'exercise index page' do
       expect(page).to have_content "Completed 1 times"
     end
   end
+  it 'when I execute an incorrect solution, it is not marked as complete' do
+    create(:user)
+    Student.create!(name: 'b')
+    user = create(:user)
+    ex1 = Exercise.create!(name: "all", solution: "Student.all", instruction: "Write a query to return all Student models from the database.", index: 3)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user.reload)
+    visit exercise_path(ex1)
+    fill_in :solution_solution_code, with: "Course.all"
+    click_on 'execute'
+    expect(page).to_not have_content('Success!')
+    visit exercises_path
+    within "#exercise-#{ex1.id}" do
+      expect(page).to_not have_content "Completed"
+    end
+  end
 end
